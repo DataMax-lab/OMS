8adc6947d225a256734996acf5f1c5384f5c4475
Yogesh Ladhe <yogesh@sportzinteractive.com>
2024-10-05 08:16:10 +0530

    add raw layer

---
+++ raw_layer.py
@@ -0,0 +1,9 @@
+import duckdb
+import pandas as pd
+
+emp_path = r"D:\DataMax\Data\emp.csv"
+df  = pd.read_csv(emp_path)
+
+df_count = duckdb.sql("select count(1) from df")
+df_count =df_count.to_df()
+print(df_count)
